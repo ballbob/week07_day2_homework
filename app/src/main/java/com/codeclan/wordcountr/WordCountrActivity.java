eb309f4d28af691faef435928b659563c064703a
Robert Ball <robert.ball.1989@gmail.com>
2017-03-14 16:21:18 +0000

    Created WordCountrActivity, the main page.

--- app/src/main/java/com/codeclan/wordcountr/WordCountrActivity.java
+++ app/src/main/java/com/codeclan/wordcountr/WordCountrActivity.java
@@ -2,10 +2,14 @@ package com.codeclan.wordcountr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class WordCountrActivity extends AppCompatActivity {
 
+    EditText textToBeCounted;
+    Button countButton;
     TextView enteredText;
     TextView wordCount;
 
@@ -13,7 +17,8 @@ public class WordCountrActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_countr);
-        
+
+        textToBeCounted = (EditText)findViewById(R.id.text_to_be_counted)
 
     }
 }
